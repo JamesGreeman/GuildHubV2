04f60f570617f21e9343b4bc7cbc8b7db85b7be7
James Greeman <jamie.greeman@gmail.com>
2015-01-25 22:30:34 +0000

    Fleshing out some classes

---
+++ src/java/com/zanvork/guildhub/controllers/GuildRequest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zanvork.guildhub.controllers;
+import com.zanvork.utils.RequestMap;
+/**
+ *
+ * @author Jamie
+ */
+public class GuildRequest extends Request{
+    
+    public GuildRequest(RequestMap request){
+        super(request);
+    }
+    
+    public String processRequest(){
+        return "";
+    }
+}
